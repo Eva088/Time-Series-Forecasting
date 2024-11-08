dfa6254293c6f43aac09c72ad4bf8b8f670c581f
Eva R. Pandey <pujan.rijal@gmail.com>
2024-11-08 14:09:50 -0800

    Created a time series plot of hardcover sales

--- Time Series Forecasting/Kaggle Time Series Course/LinearRegressionWithTimeSeries.py
+++ Time Series Forecasting/Kaggle Time Series Course/LinearRegressionWithTimeSeries.py
@@ -24,6 +24,37 @@ print(df.head())
 
 #Creating a time plot where Time forms the x-axis
 
+#Modeling Time Dependence
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.style.use('seaborn-v0_8-whitegrid') #styling matplotlib graphs with seaborn
+
+#set the default parameters for Matplotlib; customise the appearance of the plots globally, without having to modify each plot individually
+plt.rc(
+    'figure',
+    autolayout = True, 
+    figsize = (11,4),
+    titlesize = 18,
+    titleweight = 'bold'
+)
+
+plt.rc(
+    'axes',
+    labelweight = 'bold',
+    labelsize = 'large', 
+    titleweight = 'bold',
+    titlesize = 16, 
+    titlepad = 10
+)
+
+fig,ax = plt.subplots()
+ax.plot('Time', 'Hardcover', data = df, color = '0.75') #creates a lineplot
+ax = sns.regplot(x = 'Time', y = 'Hardcover', data = df, ci = None, scatter_kws = dict(color = '0.25')) #creates a scatterplot with a regression line 
+ax.set_title('Time Plot of Hardcover Sales')
+
+
+
 
 
 
