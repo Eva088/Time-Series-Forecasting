0b137a6397d64e1b9cd0b5166e9d80cc07c400c6
Eva R. Pandey <pujan.rijal@gmail.com>
2024-11-08 10:35:03 -0800

    Creating a dataframe with Date as the Index Column  recording the Hardcover sales over 30 days

--- test.py
+++ test.py
@@ -1,10 +1,5 @@
 import pandas as pd
+df = pd.read_csv("../Kaggle-Projects/Time Series Forecasting/Kaggle Time Series Course/book_sales.csv",
+index_col = 'Date'). drop('Paperback', axis = 1)
 
-df = pd.read_csv(
-    "/Users/eva/Desktop/Kaggle/Kaggle Time Series Course/book_sales.csv",
-    index_col='Date',
-    parse_dates=['Date'],
-).drop('Paperback', axis=1)
-
-df.head()
 print(df)
