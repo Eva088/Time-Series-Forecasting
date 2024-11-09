b4f9d74e5cc8384cdb1e9556f4f4079eeb731fd7
Eva R. Pandey <pujan.rijal@gmail.com>
2024-11-08 16:15:01 -0800

    Time Plot of Number of Vehicles

---
+++ Time Series Forecasting/Kaggle Time Series Course/Tunnel.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#Created a dataframe with a time dummy column 
+df = pd.read_csv('/Users/eva/Kaggle-Projects/Time Series Forecasting/Kaggle Time Series Course/Tunnel.csv', 
+index_col= 'Day')
+
+df['Time'] = np.arange(len(df.index))
+
+print(df)
+
+#Creating a time plot showing trend in the Number of Vehicles over time
+
+plt.style.use('seaborn-v0_8-darkgrid')
+plt.rc(
+    'figure',
+    autolayout = True, 
+    figsize = (10, 8), 
+    titlesize = 18,
+    titleweight = 'bold'
+)
+
+plt.rc(
+    'axes',
+    labelweight = 'bold',
+    labelsize = 'large',
+    titleweight = 'bold',
+    titlesize = 16,
+    titlepad = 10
+
+)
+
+fig, ax = plt.subplots()
+# ax.plot('Time', 'NumVehicles', data = df, color = '0.75')
+ax = sns.regplot( x = 'Time', y = 'NumVehicles', data = df, ci = None, scatter_kws = dict(color = '0.25'), line_kws = dict(color = 'r'))
+ax.set_title('Time Plot of Number of Vehicles')
+
+
+
