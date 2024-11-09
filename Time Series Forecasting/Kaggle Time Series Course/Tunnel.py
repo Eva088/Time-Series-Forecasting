3dd7c1b204ad47d4112b8668e028e2c786a8b8b9
Eva R. Pandey <pujan.rijal@gmail.com>
2024-11-08 16:33:38 -0800

    Lag Plot of Number of Vehicles

--- Time Series Forecasting/Kaggle Time Series Course/Tunnel.py
+++ Time Series Forecasting/Kaggle Time Series Course/Tunnel.py
@@ -37,5 +37,16 @@ fig, ax = plt.subplots()
 ax = sns.regplot( x = 'Time', y = 'NumVehicles', data = df, ci = None, scatter_kws = dict(color = '0.25'), line_kws = dict(color = 'r'))
 ax.set_title('Time Plot of Number of Vehicles')
 
+#Lag Plot showing correlation of number of vehicles compared to the previous day
 
+df ['lag_1'] = df ['NumVehicles'].shift(1)
+df = df.reindex(columns = ['NumVehicles', 'lag_1'])
+print(df)
 
+fig, ax = plt.subplots()
+ax = sns.regplot(x = 'lag_1', y = 'NumVehicles', data = df, scatter_kws = dict(color = '0.25'), line_kws = dict(color = 'r'), ci = None)
+ax.set_aspect(1)
+ax.set_title('Lag Plot of Number of Vehicles')
+
+#%%
+plt.show()
