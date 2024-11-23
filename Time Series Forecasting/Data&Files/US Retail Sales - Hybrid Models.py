c46439e27c16719b0ed9141cb06081d39eb7c093
Eva R. Pandey <pujan.rijal@gmail.com>
2024-11-22 16:26:27 -0800

    Setting Matplotlib defaults

--- Time Series Forecasting/Data&Files/US Retail Sales - Hybrid Models.py
+++ Time Series Forecasting/Data&Files/US Retail Sales - Hybrid Models.py
@@ -11,3 +11,29 @@ from xgboost import XGBRegressor
 
 simplefilter('ignore')
 
+#Setting Matplotlib defaults
+
+plt.style.use("seaborn-whitegrid")
+plt.rc(
+    "figure",
+    autolayout=True,
+    figsize=(11, 4),
+    titlesize=18,
+    titleweight='bold',
+)
+plt.rc(
+    "axes",
+    labelweight="bold",
+    labelsize="large",
+    titleweight="bold",
+    titlesize=16,
+    titlepad=10,
+)
+plot_params = dict(
+    color="0.75",
+    style=".-",
+    markeredgecolor="0.25",
+    markerfacecolor="0.25",
+)
+
+
