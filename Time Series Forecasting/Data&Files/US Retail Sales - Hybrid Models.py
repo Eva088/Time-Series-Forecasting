021da97a8f708e0b415bab2651bbcc661cd723f6
Eva R. Pandey <pujan.rijal@gmail.com>
2024-11-22 16:11:54 -0800

    Created a new file for documenting hybrid models

---
+++ Time Series Forecasting/Data&Files/US Retail Sales - Hybrid Models.py
@@ -0,0 +1,2 @@
+#This project involves creating a linear regression + XGBoost hybrid to forecast US Retail sales in the years from 2016-19 using the sales data from 1992 to 2019, as collected by the US Census Bureau. 
+
