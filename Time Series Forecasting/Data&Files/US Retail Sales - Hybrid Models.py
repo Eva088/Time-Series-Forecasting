d15521a75406dd447a75f4b586ed935dd2b86ac1
Eva R. Pandey <pujan.rijal@gmail.com>
2024-11-22 16:18:26 -0800

    Importing the necessary libraries

--- Time Series Forecasting/Data&Files/US Retail Sales - Hybrid Models.py
+++ Time Series Forecasting/Data&Files/US Retail Sales - Hybrid Models.py
@@ -1,2 +1,13 @@
 #This project involves creating a linear regression + XGBoost hybrid to forecast US Retail sales in the years from 2016-19 using the sales data from 1992 to 2019, as collected by the US Census Bureau. 
 
+from pathlib import Path
+from warnings import simplefilter
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from statsmodels.tsa.deterministic import CalendarFourier, DeterministicProcess
+from xgboost import XGBRegressor
+
+simplefilter('ignore')
+
