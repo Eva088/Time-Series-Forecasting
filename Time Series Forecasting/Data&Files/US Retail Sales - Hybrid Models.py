b5743449a233a521adf841f4283be8900caecd0f
Eva R. Pandey <pujan.rijal@gmail.com>
2024-11-25 13:34:12 -0800

    Fitting the XGBoost Model and plotting

--- Time Series Forecasting/Data&Files/US Retail Sales - Hybrid Models.py
+++ Time Series Forecasting/Data&Files/US Retail Sales - Hybrid Models.py
@@ -80,7 +80,6 @@ model.fit(X_train,y_train)
 
 y_fit = pd.DataFrame(model.predict(X_train), index = y_train.index, columns = y_train.columns)
 
-print(y_fit.head())
 y_pred = pd.DataFrame(model.predict(X_test), index = y_test.index, columns = y_test.columns)
 
 axs = y_train.plot(color = '0.25', subplots = True, sharex = True)
@@ -89,4 +88,72 @@ axs = y_fit.plot(color = 'C0', subplots = True, sharex = True, ax = axs)
 axs = y_pred.plot(color = 'C3', subplots = True, sharex = True, ax = axs)
 
 for ax in axs: ax.legend([])
- _ = plt.suptitle("Trends")
+_ = plt.suptitle("Trends")
+
+
+#Converting the series from a wide ( one time series per column ) to a long format (series indexed by categories along rows)
+ 
+X = retail.stack()
+
+y = X.pop('Sales')
+
+X = X.reset_index('Industries')
+
+#Label encoding for 'Industries' feature
+
+for colname in X.select_dtypes(['object','category']):
+    X[colname], _ = X[colname].factorize() #encoded values, unique values 
+    
+#Label encoding for annual seasonality
+
+X['Month'] = X.index.month
+
+#Creating splits
+
+X_train, X_test = X.loc[idx_train,:], X.loc[idx_test,:]
+y_train, y_test = y.loc[idx_train,:],y.loc[idx_test,:]
+
+
+#Converting the trend predictions into a long format and then subtracting them from the original series to create a detrended(residuals) series that XGBoost can learn. 
+
+y_fit = y_fit.stack().squeeze()
+y_pred = y_pred.stack().squeeze()
+
+
+#Creating residuals (the collection of detrended series) from the training set
+
+y_resid = y_train - y_fit
+
+#Training XGBoost on the residuals
+
+xgb = XGBRegressor()
+xgb.fit(X_train, y_resid)
+
+#Adding the predicted residuals onto the predicted trends
+
+y_fit_boosted = xgb.predict(X_train) + y_fit
+y_pred_boosted = xgb.predict(X_test) + y_pred
+
+axs = y_train.unstack(['Industries']).plot(
+    color='0.25', figsize=(11, 5), subplots=True, sharex=True,
+    title=['BuildingMaterials', 'FoodAndBeverage'],
+)
+
+axs = y_test.unstack(['Industries']).plot(
+    color='0.25', subplots=True, sharex=True, ax=axs,
+)
+
+axs = y_fit_boosted.unstack(['Industries']).plot(
+    color='C0', subplots=True, sharex=True, ax=axs,
+)
+
+axs = y_pred_boosted.unstack(['Industries']).plot(
+    color='C3', subplots=True, sharex=True, ax=axs,
+)
+
+for ax in axs: ax.legend([])
+
+#%%
+plt.show()
+
+#We can see that the trend learned by XGBoost is only as good as the trend learned by the linear regression -- in particular, XGBoost wasn't able to compensate for the poorly fit trend in the 'BuildingMaterials' series.
